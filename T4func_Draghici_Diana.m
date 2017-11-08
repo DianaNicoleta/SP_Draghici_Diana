afa505c1a85eba73a245e9d9b843e369e8aaef6d
DianaNicoleta <32995108+DianaNicoleta@users.noreply.github.com>
2017-11-08 18:52:31 +0200

    tema4func

---
+++ T4func_Draghici_Diana.m
@@ -0,0 +1,16 @@
+function[]=T4func_Draghici_Diana(t,nr)
+
+A=0.8;
+T=3;
+figure(nr)
+
+%inmultim functia cu amplitudinea iar argumentul sinusului il impartim la
+%perioada semnalului neredresat pentru a obtine graficul dorit
+s=A*sin(2*pi*t*1/T);
+
+%Monoredresare (toate elementele mai mici decat 0 sunt transformate in 0)
+s(s<0) = 0;
+
+plot(t, s, '-r.'),title('Semnal sinusoidal redresat mono alternanta'),xlabel('Timp [s]'),ylabel('A [V]')
+
+end

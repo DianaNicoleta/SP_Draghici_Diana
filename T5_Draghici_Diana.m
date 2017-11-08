b3adb20e55e8369390a3c47e3552c1cf9d15bebd
DianaNicoleta <32995108+DianaNicoleta@users.noreply.github.com>
2017-11-08 18:52:42 +0200

    tema5

---
+++ T5_Draghici_Diana.m
@@ -0,0 +1,6 @@
+T = 8;
+
+%in functie de rezolutia temporala,introduc
+T5func_Draghici_Diana(0:0.002:T,1) % 2ms = 0.002s
+T5func_Draghici_Diana(0:0.02:T,2) % 20ms = 0.02s
+T5func_Draghici_Diana(0:0.2:T,3) % 200ms = 0.2s

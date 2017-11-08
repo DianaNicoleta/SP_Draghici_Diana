c8328740fab96162608f106683f34eba7005a99b
DianaNicoleta <32995108+DianaNicoleta@users.noreply.github.com>
2017-11-08 18:52:52 +0200

    tema5func

---
+++ T5func_Draghici_Diana.m
@@ -0,0 +1,12 @@
+function[]=T5func_Draghici_Diana(t,nr)
+
+A=1.5;
+T=4;
+figure(nr)
+
+%inmultim functia in modul cu amplitudinea iar argumentul sinusului il impartim la
+%perioada semnalului neredresat pentru a obtine graficul dorit (redresare)
+s=A*abs(sin(2*pi*t*1/T));
+plot(t, s, '-r.' ),title('Semnal sinusoidal redresat dubla alternanta'),xlabel('Timp [s]'),ylabel('A [V]')
+
+end

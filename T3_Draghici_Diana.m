e0d2500154ecfbbce69153d59eda9f1ba8e2d81f
DianaNicoleta <32995108+DianaNicoleta@users.noreply.github.com>
2017-11-08 18:51:36 +0200

    tema3

---
+++ T3_Draghici_Diana.m
@@ -0,0 +1,6 @@
+T = 15;
+
+%in functie de rezolutia temporala,introduc:
+T3func_Draghici_Diana(0:0.002:T,1) % 2ms = 0.002s
+T3func_Draghici_Diana(0:0.02:T,2) % 20ms = 0.02s
+T3func_Draghici_Diana(0:0.2:T,3) % 200ms = 0.2s

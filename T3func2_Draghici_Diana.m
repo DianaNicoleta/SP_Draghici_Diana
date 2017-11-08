1a742f17c8a63938042eb616caaad6422cd0e17e
DianaNicoleta <32995108+DianaNicoleta@users.noreply.github.com>
2017-11-08 18:52:05 +0200

    tema3 a doua functie

---
+++ T3func2_Draghici_Diana.m
@@ -0,0 +1,12 @@
+function[]=T3func2_Draghici_Diana(t,nr,niv,k)
+
+figure((nr-1)*4+k)
+hold on
+title('Semnal dreptunghiular multinivel aleator'),xlabel('Timp [s]'),ylabel('A [V]')
+%cu functia datasample procuram observatii despre vectorul niv
+%functia rectpuls genereaza un impuls unitate dreptunghiular de durata 1/4
+for n=0:0.25:40
+    y = datasample(niv, 1);
+    plot(t, y*rectpuls(t-n, 0.25))
+end
+end

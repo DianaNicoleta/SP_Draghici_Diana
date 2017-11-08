8fc9ac98d6139324389d8856d4ec83bfd8c6be5a
DianaNicoleta <32995108+DianaNicoleta@users.noreply.github.com>
2017-11-08 18:52:16 +0200

    tema4

---
+++ T4_Draghici_Diana.m
@@ -0,0 +1,6 @@
+T = 4;
+
+%in functie de rezolutia temporala,introduc:
+T4func_Draghici_Diana(0:0.002:T,1) % 2ms = 0.002s
+T4func_Draghici_Diana(0:0.02:T,2) % 20ms = 0.02s
+T4func_Draghici_Diana(0:0.2:T,3) % 200ms = 0.2s
